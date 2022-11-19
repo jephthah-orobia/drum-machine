34e1cc7cdb68ed18d8eaf8148204a525b5dffef4
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-11-19 12:22:55 +0800

    Update DropDown.jsx
    remove logger

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class DropDown extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
     if (
       "options" in props &&
       "selectedIndex" in props &&
